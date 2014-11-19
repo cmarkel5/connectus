da3f6f4911dbc1a17f171a24f4665824daf56aa0
cmarkel5 <cmarkel5@gmail.com>
2014-11-19 11:34:33 -0500

    working geocoder gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ActiveRecord::Base
+  def complete_address
+    "#{street_address}, #{city}, #{state}, #{zip_code}"
+  end
+
+  geocoded_by :complete_address
+  after_validation :geocode, :if => :street_address_changed? ||
+                                    :city_changed? ||
+                                    :state_changed? ||
+                                    :zip_code_changed?
 end
