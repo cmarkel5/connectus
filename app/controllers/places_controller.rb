ca81e4ee1fe60bdb32990c1f3a4d59f72da0f23e
amandawraymond <amandawraymond@gmail.com>
2014-11-20 19:39:38 -0500

    edits to see if maps can render selected users markers

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -5,9 +5,14 @@ class PlacesController < ActionController::Base
   def index
     # Default to params, second is test, third is realsies
     location = params[:location] || "38.8977332,-77.0365305" || "33.771527,-84.367367"
-    search_url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=AIzaSyATtqKSdyRFCZNA1Lce8OxV3_TD-NEyxaA&location=#{location}&rankby=distance&keyword=free%20wifi&types=bakery|bar|book_store|cafe|food|library|restaurant"
+    search_url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=AIzaSyATtqKSdyRFCZNA1Lce8OxV3_TD-NEyxaA&location=#{location}&rankby=distance&keyword=wifi&types=bakery|bar|book_store|cafe|food|library|restaurant"
+    # search_url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=AIzaSyATtqKSdyRFCZNA1Lce8OxV3_TD-NEyxaA&location=#{location}&rankby=distance&keyword=free%20wifi&types=bakery|bar|book_store|cafe|food|library|restaurant"
     search_url = URI::encode(search_url)
     places = HTTParty.get search_url
     @places = places["results"].uniq
   end
 end
+
+
+    
+ 
