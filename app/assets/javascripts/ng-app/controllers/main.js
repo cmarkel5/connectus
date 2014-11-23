a5e93e9d7d285c9009cd4e5932ca659a6876c535
amandawraymond <amandawraymond@gmail.com>
2014-11-23 00:20:42 -0500

    maps markers dynamic,window re-sized,midpoint marker dynamic and blue

--- app/assets/javascripts/ng-app/controllers/main.js
+++ app/assets/javascripts/ng-app/controllers/main.js
@@ -1,4 +1,4 @@
-'use strict';
+
 
 /**
  * @ngdoc function
@@ -7,59 +7,135 @@
  * # MainCtrl
  * Controller of the connectusAngular2App
  */
+
 angular.module('connectusApp')
-  .controller('MainCtrl', ['$scope', 'uiGmapGoogleMapApi',
-              function ($scope, uiGmapGoogleMapApi) {
+  .controller('MainCtrl', ['$scope', '$log', 'groupService', 'placesService', 'uiGmapGoogleMapApi',
+              function ($scope, $log, groupService, placesService, uiGmapGoogleMapApi) {
     
     console.log('MainCtrl is alive!');
 
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
 
-    var midPointLat = (33.882147 + 33.773164)/2;
-    var midPointLong = (-84.521571 + -84.288918)/2;
 
-    $scope.map = {
-      center: {
-        latitude: midPointLat,
-        longitude: midPointLong
-      },
-      zoom: 10
-    };
+    // var midPointLat = (33.882147 + 33.773164)/2;
+    // var midPointLong = (-84.521571 + -84.288918)/2;
+
+    // $scope.map = {
+    //   center: {
+    //     latitude: midPointLat,
+    //     longitude: midPointLong
+    //   },
+    //   zoom: 10
+    // };
 
     uiGmapGoogleMapApi.then(function(/* maps */) {
       // can manipulate the map here.
     });
 
-  $scope.markerList = [
-    // {
-    //   id: 0,
-    //   coords: {
-    //     latitude: 33.771527,
-    //     longitude: -84.367367
-    //   },
-    //   message: 'General Assembly'
-    // },
-    {
-      id: 1,
-      coords: {
-        latitude: 33.882147,
-        longitude: -84.521571
-      },
-      message: 'Chris Markel'
-      // icon:"https://en.gravatar.com/userimage/74172252/39d87c7514bfdc6e8bcf4d7b8fc35472.jpg"
-    },
-    {
-      id: 2,
-      coords: {
-        latitude: 33.773164,
-        longitude: -84.288918
-      },
-      message:'Amanda Raymond'
-    }
-  ]
+    $scope.getUsers = function() {
+      groupService.getAllUsers().success(function(data) {
+        $scope.userList = data;
+        }).error(function() {
+        alert('Something went wrong!');
+      });
+    };
+
+    $scope.getUsers();
+    
+   
+    $scope.selectedUsers = function() {
+      //this line resets the userList so you can update correctly
+      var users = $scope.userList;
+      //this line filter out for users that were selected upon a submit click
+      users = _.filter(users, function(user) {
+        return user.selected === true;
+        /* alert(users.length)*/
+      });
+      //this function creates an array of just what you want.
+
+      /*$scope.submitForm = function () {
+        alert("send a request tonthe server: " + JSON.stringify(users));
+       };*/
+      var length = users.length;
+     
+      var sumLatitude = _.reduce( users, function( memory, user) {
+        return memory + user.latitude;
+      }, 0 );
+      
+      var averageLatitude = sumLatitude/length;
+    
+      var sumLongitude = _.reduce( users, function( memory, user) {
+        return memory + user.longitude;
+      }, 0 );
+      
+      var averageLongitude = sumLongitude/length;
+
+      $scope.midPoint = averageLatitude + "," + averageLongitude;
+
+      $scope.getPlaces();
+
+
+      // var midPointLat = (33.882147 + 33.773164)/2;
+      // var midPointLong = (-84.521571 + -84.288918)/2;
+     
+      $scope.map = {
+        center: {
+          latitude: averageLatitude,
+          longitude: averageLongitude
+        },
+        zoom: 10
+      };
+      $scope.searchbox = { template:'searchbox.tpl.html', position:'top-left'};
+
+      $scope.midPointMarker = [
+        { id: 0,
+         coords: {
+            latitude: averageLatitude,
+            longitude: averageLongitude
+          },
+          icon: { url:"http://www.clker.com/cliparts/c/I/g/P/d/h/google-maps-pin-blue-th.png",
+                  scaledSize: {height: 40,
+                         width: 40
+                        }
+                },
+          name: "Midpoint"
+        }];
+
+     
+
+      $scope.getAllMarkers = function() {
+        $scope.markerList = users;
+      };
+      $scope.getAllMarkers();
+
+    };
+
+    
+    
+
+
+    $scope.getPlaces = function() {
+      placesService.getAllPlaces($scope.midPoint).success(function(data) {
+        $scope.placesHash = data;
+      }).error(function() {
+        alert('Something went wrong!');
+      });
+    };
+
+    
+    //move this filter to rails side in places controller
+    $scope.places = _.filter($scope.placesHash, function(place) {
+        return place.opening_hours.open_now === true && !_.contains(place.types, "lodging");
+    });
+
+    $scope.selectPlace = function(place) {
+      $scope.selectedPlace = place;
+    };
+
+    $scope.clearSelectedPlace = function() {
+      $scope.selectedPlace = null;
+    };
+
+  
+ 
 
   }]);
