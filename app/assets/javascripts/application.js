db4fff6b3ad64f7026650002024e623aa37c9dc5
cmarkel5 <cmarkel5@gmail.com>
2014-11-24 15:14:54 -0500

    mobile responsive bootstrapping

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap-sass-official/assets/javascripts/bootstrap-sprockets.js
 //= require angular
 //= require angular-animate
 //= require angular-bootstrap
